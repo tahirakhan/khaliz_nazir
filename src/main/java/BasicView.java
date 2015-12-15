10c7a8a3c0f1dab6ab62056db27f615f516ffb6f
Tahir Ali Khan <tahir.ali@vroozi.com>
2015-12-15 14:45:57 +0500

    init

---
+++ src/main/java/BasicView.java
@@ -0,0 +1,43 @@
+/**
+ * Created by Tahir Ali Khan on 12/15/2015.
+ */
+import java.io.Serializable;
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+import javax.faces.bean.ViewScoped;
+import org.primefaces.model.TreeNode;
+
+
+@ManagedBean(name="ttBasicView")
+@ViewScoped
+public class BasicView implements Serializable {
+
+  private TreeNode root;
+
+  private Document selectedDocument;
+
+  @ManagedProperty("#{documentService}")
+  private DocumentService service;
+
+  @PostConstruct
+  public void init() {
+    root = service.createDocuments();
+  }
+
+  public TreeNode getRoot() {
+    return root;
+  }
+
+  public void setService(DocumentService service) {
+    this.service = service;
+  }
+
+  public Document getSelectedDocument() {
+    return selectedDocument;
+  }
+
+  public void setSelectedDocument(Document selectedDocument) {
+    this.selectedDocument = selectedDocument;
+  }
+}
